bc057119485ced94968f9c072403b6a8cd9cd554
sivakrishna mallampati <62555465+shva9959@users.noreply.github.com>
2020-06-15 13:17:15 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,1 @@
-from django.db import models
-from django.utils import timezone
-
-
-class Vehicle(models.Model):
-    v_id = models.PositiveIntegerField(null=True)
-    dtstamp = models.DateTimeField(default=timezone.now)
-    v_regnNo = models.CharField(max_length=20, blank=False, null=False)
-    v_model = models.CharField(max_length=20, blank=False, null=False)
-    v_make = models.CharField(max_length=20, blank=False, null=False)
-
-    def __str__(self):
-        return self.v_id
+print("hilo")
